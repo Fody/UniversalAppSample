f568019d47961097132ef586b2e5b1abb0c2c275
Simon Cropp <simon.cropp@gmail.com>
2015-08-04 06:52:17 +1000

    duplicate

---
+++ AppWithFody/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace AppWithFody
+{
+
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = new Frame();
+            rootFrame.NavigationFailed += OnNavigationFailed;
+            Window.Current.Content = rootFrame;
+            rootFrame.Navigate(typeof(MainPage));
+            Window.Current.Activate();
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+    }
+}
---
+++ AppWithFody/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace AppWithFody
+{
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = new Person();
+        }
+    }
+}
